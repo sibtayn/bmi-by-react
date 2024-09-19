1e4a2f39a540ddc241d7f31104b46e5b4ce90b4f
sibtayn <161344158+sibtayn@users.noreply.github.com>
2024-09-19 19:20:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,109 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+  // const [count, setCount] = useState(0)
+
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.jsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
+// }
+
+// export default App
+
+
+
+import './App.css';
+import './index.css'
+import React, {useState} from 'react'
+ 
+function App() {
+ 
+  // state
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+ 
+  let calcBmi = (event) => {
+    //prevent submitting to the server
+    event.preventDefault()
+ 
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+ 
+      // Logic for message
+ 
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+ 
+ 
+  let reload = () => {
+    window.location.reload()
+  }
+ 
+  return (
+    <div className="app">
+    <div className='container'>
+      <h2 className='center'>BMI Calculator</h2>
+      <form onSubmit={calcBmi}>
+ 
+        <div>
+          <label>Weight (lbs)</label>
+          <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+        </div>
+ 
+        <div>
+          <label>Height (in)</label>
+          <input value={height} onChange={(event) => setHeight(event.target.value)} />
+        </div>
+ 
+        <div>
+          <button className='btn' type='submit'>Submit</button>
+          <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+        </div>
+      </form>
+ 
+      <div className='center'>
+        <h3>Your BMI is: {bmi}</h3>
+        <p>{message}</p>
+      </div>
+    </div>
+  </div>
+  );
+}
+ 
+export default App;
